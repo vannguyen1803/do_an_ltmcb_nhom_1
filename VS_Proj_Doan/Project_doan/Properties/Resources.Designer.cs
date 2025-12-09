fb7603a1b33318ed6080796bd9750a05cb050420
Trantran365 <trantran128980@gmail.com>
2025-12-09 20:44:49 +0700

    Chỉnh sửa

--- VS_Proj_Doan/Project_doan/Properties/Resources.Designer.cs
+++ VS_Proj_Doan/Project_doan/Properties/Resources.Designer.cs
@@ -69,25 +69,5 @@ namespace Project_doan.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap pause {
-            get {
-                object obj = ResourceManager.GetObject("pause", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap play_button {
-            get {
-                object obj = ResourceManager.GetObject("play-button", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
